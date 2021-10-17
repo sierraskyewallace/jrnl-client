4e8ddcdadcac4602bde0d47ec939b9554240276e
sierraskyewallace <sskwallace@gmail.com>
2021-10-17 12:21:00 -0700

    added dev tools, thunk

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 
+
 class App extends React.Component {
 
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+
+
+ReactDOM.render(
+  <Provider>
+  <App />
+  </Provider >
+  ,
+  document.getElementById('root'));
 
 
